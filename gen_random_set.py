457a2269d52b8d2db85d5e35fd03350dfc68abc6
bcen707 <czarsalad707@gmail.com>
2022-08-24 15:06:46 -0700

    added existing file

---
+++ gen_random_set.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 24 14:46:38 2022
+
+@author: billy
+"""
+
+
+bootstrap_loandf= []
+
+#code base to be iterated
+x= np.random.choice(hmeq['LOAN'], 5)
+bootstrap_loandf.append(x)
