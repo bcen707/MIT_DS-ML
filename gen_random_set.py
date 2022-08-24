be170468ab91e7cbc4a1ad9041aa598a1967ea1b
bcen707 <czarsalad707@gmail.com>
2022-08-24 15:26:19 -0700

    encapsulated in a function and factor out size and dataset parameters

--- gen_random_set.py
+++ gen_random_set.py
@@ -9,6 +9,14 @@ Created on Wed Aug 24 14:46:38 2022
 
 bootstrap_loandf= []
 
-for i in range(2): 
-    x= np.random.choice(hmeq['LOAN'], 5)
-    bootstrap_loandf.append(x)
+def test_gen_random_set(dataframe, size):
+    for i in range(2):
+        sample= np.random.choice(dataframe, size)
+        bootstrap_loandf.append(sample)
+        
+        
+# Test Code
+
+# test_gen_random_set(hmeq['LOAN'], 0)
+# test_gen_random_set(hmeq['LOAN'], 5)
+# test_gen_random_set(hmeq['LOAN'], -1)
